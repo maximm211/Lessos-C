6dafbb3451cd9ced1ff5894427b1462e45ec446a
maximm211 <71208702+maximm211@users.noreply.github.com>
2020-09-13 15:33:01 +0600

    First commet

---
+++ Lesson/Lesson/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            int a = 4;
+            int b = 6;
+
+            Console.WriteLine("Instal a :" + a);
+            Console.WriteLine("Instal b :" + b);
+
+            b = a;
+
+            Console.WriteLine("After b=a is :" + b);
+
+            a = 400;
+
+            Console.WriteLine("Final a :" + a);
+            Console.WriteLine("Final b :" + b);
+
+            Console.WriteLine("---------------------------------------------------------------------------");
+
+            List<int> list1 = new List<int>() { 1, 2, 3, 4, 4 };
+            List<int> list2;
+
+            list2 = list1;
+
+            Console.WriteLine(list1[2]);
+            Console.WriteLine(list2[2]);
+
+            list1.RemoveAt(0);
+
+            Console.WriteLine(list1[2]);
+            Console.WriteLine(list2[2]);
+
+            Console.ReadLine();
+        }
+    }
+}
